4d73ce9403e50f6f7a6b8addf81a0e00de63b322
mohit3011 <mohit.chandra@research.iiit.ac.in>
2017-08-16 03:38:46 +0530

    Add client code with message passing with threading

--- client/Client.java
+++ client/Client.java
@@ -1,70 +1,70 @@
-// A Java program for a Client
-import java.net.*;
 import java.io.*;
- 
-public class Client
-{
-    // initialize socket and input output streams
-    private Socket socket            = null;
-    private BufferedReader input   = null;
-    private DataOutputStream out     = null;
- 
-    // constructor to put ip address and port
-    public Client(String address, int port)
-    {
-        // establish a connection
-        try
-        {
-            socket = new Socket(address, port);
-            System.out.println("Connected");
- 
-            // takes input from terminal
-            input  = new BufferedReader(new InputStreamReader(System.in));
- 
-            // sends output to the socket
-            out    = new DataOutputStream(socket.getOutputStream());
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+    
+    String hostname = "localhost";
+    int port = 6789;
+
+    // declaration section:
+    // clientSocket: our client socket
+    // os: output stream
+    // is: input stream
+    
+        Socket clientSocket = null;  
+        DataOutputStream os = null;
+        BufferedReader is = null;
+    
+    // Initialization section:
+    // Try to open a socket on the given port
+    // Try to open input and output streams
+    
+        try {
+            clientSocket = new Socket(hostname, port);
+            os = new DataOutputStream(clientSocket.getOutputStream());
+            is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: " + hostname);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: " + hostname);
         }
-        catch(UnknownHostException u)
-        {
-            System.out.println(u);
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
-        }
- 
-        // string to read message from input
-        String line = "";
- 
-        // keep reading until "Over" is input
-        while (!line.equals("Over"))
-        {
-            try
-            {
-                line = input.readLine();
-                out.writeUTF(line);
-            }
-            catch(IOException i)
-            {
-                System.out.println(i);
-            }
-        }
- 
-        // close the connection
-        try
-        {
-            input.close();
-            out.close();
-            socket.close();
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
-        }
+    
+    // If everything has been initialized then we want to write some data
+    // to the socket we have opened a connection to on the given port
+    
+    if (clientSocket == null || os == null || is == null) {
+        System.err.println( "Something is wrong. One variable is null." );
+        return;
     }
- 
-    public static void main(String args[])
-    {
-        Client client = new Client("127.0.0.1", 5000);
+
+    try {
+        while ( true ) {
+        System.out.print( "Enter the message (Close Server to stop server, Close Client to stop client): " );
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String keyboardInput = br.readLine();
+        os.writeBytes( keyboardInput + "\n" );
+
+        if (keyboardInput.equals("Close Server") || keyboardInput.equals("Close Client")) {
+            break;
+        }
+        
+        //String responseLine = is.readLine();
+        //System.out.println("Server returns its square as: " + responseLine);
+        }
+        
+        // clean up:
+        // close the output stream
+        // close the input stream
+        // close the socket
+        
+        os.close();
+        is.close();
+        clientSocket.close();   
+    } catch (UnknownHostException e) {
+        System.err.println("Trying to connect to unknown host: " + e);
+    } catch (IOException e) {
+        System.err.println("IOException:  " + e);
     }
+    }           
 }
